a55e5ac67bc3d57068fb04b281e47071be2d1eba
Pero <G00422221@atu.ie>
2025-01-07 14:00:08 +0000

    setup

---
+++ Login Service/src/main/java/com/example/cicd_note_project/CicdNoteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_note_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdNoteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdNoteProjectApplication.class, args);
+	}
+
+}
---
+++ Login Service/src/test/java/com/example/cicd_note_project/CicdNoteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_note_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdNoteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
